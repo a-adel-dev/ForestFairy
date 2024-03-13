6b355f8a37989e23969dae343311f2a7f6611666
Ahmad A. Fouad <a.adel.dev@gmail.com>
2024-03-13 11:38:33 +0200

    Add non AI player movement script

--- Assets/HuaYao/Scripts/Player.cs
+++ Assets/HuaYao/Scripts/Player.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    Rigidbody rb;
+    public float movementSpeed = 5.0f;
+    public float JumpForce = 2.0f;
+    bool grounded;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        float horizontalMovement = Input.GetAxis("Horizontal");
+        float verticalMovement = Input.GetAxis("Vertical");
+
+        rb.velocity = new Vector3(horizontalMovement, 0 , verticalMovement) * movementSpeed;
+
+
+        bool jumpButton = Input.GetButton("Jump");
+
+        if (jumpButton && grounded)
+        {
+            Debug.Log("jump button pressed");
+            rb.AddForce((Vector3.up * JumpForce),ForceMode.Impulse);
+        }
     }
 }
